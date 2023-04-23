9ec083c5f73f4c9d9f20d5c3012a06e576658db0
WanabeDev <131475192+WanabeDev@users.noreply.github.com>
2023-04-24 00:07:30 +0900

    Add files via upload
    solve: 문자열로 변환

---
+++ Level0/문자열로 변환.js
@@ -0,0 +1,19 @@
+// answer01
+function solution(n) {
+  return String(n);
+}
+
+// answer02
+function solution(n) {
+  return n + "";
+}
+
+// answer03
+function solution(n) {
+  return n.toString();
+}
+
+// answer04
+function solution(n) {
+  return `${n}`;
+}
