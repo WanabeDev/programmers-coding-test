9edf61a1931ba27c3147ac1d0ccceb5fc4be1b01
WanabeDev <wanabedep@gmail.com>
2023-05-01 16:17:19 +0900

    solve: 특정한 문자를 대문자로 바꾸기

---
+++ Level0/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,29 @@
+// answer01(map활용)
+function solution(my_string, alp) {
+  const result = [...my_string].map((v) => {
+    if (v == alp) {
+      return v.toUpperCase();
+    } else {
+      return v;
+    }
+  });
+  return result.join("");
+}
+
+// answer01(for활용)
+function solution(my_string, alp) {
+  let result = "";
+  for (let i = 0; i < my_string.length; i++) {
+    if (my_string[i] === alp) {
+      result += my_string[i].toUpperCase();
+    } else {
+      result += my_string[i];
+    }
+  }
+  return result;
+}
+
+// answer01(정규표현식 reduce 활용)
+function solution(my_string, alp) {
+  return my_string.replace(new RegExp(alp, "ig"), alp.toUpperCase());
+}
