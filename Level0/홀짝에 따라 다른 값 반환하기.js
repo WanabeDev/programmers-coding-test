e76490e352f9d41ca099d8babfa90c90153736c2
WanabeDev <wanabedep@gmail.com>
2023-05-01 21:43:15 +0900

    solve: 홀짝에 따라 다른 값 반환하기

---
+++ Level0/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,12 @@
+// answer01
+function solution(n) {
+  return Array.from({ length: n }, (_, i) => i + 1)
+    .filter((num) => (n % 2 === 0 ? num % 2 === 0 : num % 2 !== 0))
+    .reduce((acc, cur) => (n % 2 === 0 ? acc + cur ** 2 : acc + cur), 0);
+}
+
+// answer02
+function solution(n) {
+  if (n % 2 === 1) return ((n + 1) / 2) * ((n + 1) / 2);
+  else return (n * (n + 1) * (n + 2)) / 6;
+}
