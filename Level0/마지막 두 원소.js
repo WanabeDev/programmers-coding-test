4ff877b546f71893ad880978413305b2f828370a
WanabeDev <wanabedep@gmail.com>
2023-05-04 01:20:31 +0900

    solve: 마지막 두 원소

---
+++ Level0/마지막 두 원소.js
@@ -0,0 +1,21 @@
+// answer 01
+function solution(num_list) {
+  var result = [];
+  for (let i = 0; i < num_list.length; i++) {
+    result.push(num_list[i]);
+    if (i === num_list.length - 1) {
+      num_list[num_list.length - 2] < num_list[num_list.length - 1]
+        ? result.push(
+            num_list[num_list.length - 1] - num_list[num_list.length - 2]
+          )
+        : result.push(num_list[num_list.length - 1] * 2);
+    }
+  }
+  return result;
+}
+
+// answer 02
+function solution(num_list) {
+  const [a, b] = [...num_list].reverse();
+  return [...num_list, a > b ? a - b : a * 2];
+}
