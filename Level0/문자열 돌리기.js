57a1901986ef20424f7a8d2b0c2a6258b6ed816c
WanabeDev <wanabedep@gmail.com>
2023-05-05 21:59:42 +0900

    solve: 문자열 돌리기

---
+++ Level0/문자열 돌리기.js
@@ -0,0 +1,34 @@
+// answer 01
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+
+  for (let i = 0; i < str.length; i++) {
+    console.log(str.charAt(i));
+  }
+});
+
+// answer 02
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+  [...str].forEach((c) => console.log(c));
+});
