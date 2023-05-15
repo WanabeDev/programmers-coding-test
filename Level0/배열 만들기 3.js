bfd9a861006db1930f0c654e09c92e5a4573d2ed
WanabeDev <wanabedep@gmail.com>
2023-05-15 21:54:23 +0900

    solve: 배열 만들기 3

---
+++ Level0/배열 만들기 3.js
@@ -0,0 +1,6 @@
+function solution(arr, intervals) {
+  const [[a1, a2], [b1, b2]] = intervals;
+  const parts1 = arr.slice(a1, a2 + 1);
+  const parts2 = arr.slice(b1, b2 + 1);
+  return [...parts1, ...parts2];
+}
