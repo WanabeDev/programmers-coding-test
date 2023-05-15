5d0062a71f06358585b7cfaa499a928f2e9725a4
WanabeDev <wanabedep@gmail.com>
2023-05-15 17:26:05 +0900

    solve: 특별한 이차원 배열 2

---
+++ Level0/특별한 이차원 배열 2.js
@@ -0,0 +1,23 @@
+// answer 01
+function solution(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] !== arr[j][i]) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+// answer 02(01에서 조건문에 'i !== j'를 추가하여 불필요한 과정을 제거)
+function solution(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && arr[i][j] !== arr[j][i]) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
