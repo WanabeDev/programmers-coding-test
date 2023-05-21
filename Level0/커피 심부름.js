f138ccbc2c708a0e516626659b18904b54452f54
WanabeDev <wanabedep@gmail.com>
2023-05-21 20:56:21 +0900

    solve: 커피 심부름

---
+++ Level0/커피 심부름.js
@@ -0,0 +1,20 @@
+// Answer 01
+function solution(order) {
+  const regex = /ame|any/;
+  return order.reduce((cost, menu) => {
+    if (regex.test(menu)) {
+      return cost + 4500;
+    } else {
+      return cost + 5000;
+    }
+  }, 0);
+}
+
+// Answer 02(다항연산자를 이용하여 위 과정 축소)
+function solution(order) {
+  const regex = /ame|any/;
+  return order.reduce(
+    (cost, menu) => cost + (regex.test(menu) ? 4500 : 5000),
+    0
+  );
+}
