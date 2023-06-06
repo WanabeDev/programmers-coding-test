a24c8a192559f179c74dede8ebd8fa7b7e0d398a
WanabeDev <wanabedep@gmail.com>
2023-06-07 00:24:37 +0900

    solve: 나머지가 1이 되는 수 찾기

---
+++ LEVEL1/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,15 @@
+/* My Answer 01 (while이 정적인 조건문 뿐만 아니라 변수 값을 변경하거나 증감하는 표현식을 사용할 수 있다는 점을 이용) */
+function solution(n) {
+  var x = 1;
+  while (x++) {
+    if (n % x === 1) return x;
+  }
+}
+/* 
+
+
+*/
+/* Another Approach 01(재귀함수를 활용한 풀이, 조건을 만족하는 값을 찾을 때 까지 함수를 호출) */
+function solution(n, x = 0) {
+  return n % x === 1 ? x : solution(n, x + 1);
+}
